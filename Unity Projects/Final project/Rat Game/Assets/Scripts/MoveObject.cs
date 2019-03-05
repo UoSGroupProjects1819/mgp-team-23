8d6d9fd790f2003121c0fcbe49950ef0dc213f80
KaceyE <s195707@uos.ac.uk>
2019-03-05 12:36:35 +0000

    Optional object uses
    Added Lmb and Rmb option interaction however have a reoccuring problem of item being teleported to player

--- Unity Projects/Final project/Rat Game/Assets/Scripts/MoveObject.cs
+++ Unity Projects/Final project/Rat Game/Assets/Scripts/MoveObject.cs
@@ -2,40 +2,61 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveObject : MonoBehaviour {
+public class MoveObject : MonoBehaviour
+{
 
     public GameObject item;
     public GameObject tempParent;
     public Transform guide;
     public float speed;
+    public float rmbspeed;
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         item.GetComponent<Rigidbody>().useGravity = true;
- 
+
     }
 
     // Update is called once per frame
-    void OnMouseDown()
+    void Update()
     {
-        //Picks up object only when in range
-        item.GetComponent<Rigidbody>().useGravity = false;
-        item.GetComponent<Rigidbody>().isKinematic = true;
-        item.transform.position = guide.transform.position;
-        item.transform.rotation = guide.transform.rotation;
-        item.transform.parent = tempParent.transform;
+        if (Input.GetMouseButtonDown(0))
+        {
+            item.GetComponent<Rigidbody>().useGravity = false;
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            item.transform.position = guide.transform.position;
+            item.transform.rotation = guide.transform.rotation;
+            item.transform.parent = tempParent.transform;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            item.transform.parent = null;
+            item.transform.position = guide.transform.position;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            item.GetComponent<Rigidbody>().useGravity = false;
+            item.GetComponent<Rigidbody>().isKinematic = true;
+            item.transform.position = guide.transform.position;
+            item.transform.rotation = guide.transform.rotation;
+            item.transform.parent = tempParent.transform;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * rmbspeed;
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            item.transform.parent = null;
+            item.transform.position = guide.transform.position;
+        }
     }
-
-
-    void OnMouseUp()
-    {
-
-        //Applies force then releases the child
-        guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
-        item.GetComponent<Rigidbody>().useGravity = true;
-        item.GetComponent<Rigidbody>().isKinematic = false;
-        item.transform.parent = null;
-        item.transform.position = guide.transform.position;
-    }
-
 }
+
+
