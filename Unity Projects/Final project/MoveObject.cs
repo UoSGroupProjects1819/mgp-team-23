0abb206215feeeb2dfb0684d94c03d9f5697cf57
KaceyE <43639445+KaceyE@users.noreply.github.com>
2019-03-13 14:28:50 +0000

    Added scripts from jam 13/3

---
+++ Unity Projects/Final project/MoveObject.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+
+    public GameObject tempParent;
+    public Transform guide;
+    public float speed;
+    public float rmbspeed;
+    private bool clicked = false;
+    private GameObject item;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider != null & hit.collider.gameObject.tag == "item")
+                {
+                    item = hit.collider.gameObject;
+
+                    clicked = true;
+                    item.GetComponent<Rigidbody>().useGravity = false;
+                    item.GetComponent<Rigidbody>().isKinematic = true;
+                    item.transform.position = guide.transform.position;
+                    item.transform.rotation = guide.transform.rotation;
+                    item.transform.parent = tempParent.transform;                  
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            clicked = false;
+            guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            item.transform.parent = null;
+            item.transform.position = guide.transform.position;
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            clicked = false;
+            guide.GetChild(0).gameObject.GetComponent<Rigidbody>().velocity = transform.forward * rmbspeed;
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            item.transform.parent = null;
+            item.transform.position = guide.transform.position;
+        }
+    }
+}
+
+
+
---
+++ Unity Projects/Final project/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody Rigid;
+    public float MouseSensitivity;
+    public float MoveSpeed;
+   // public float JumpForce;
+    public GameObject playerCamera;
+
+    Quaternion _initialOrientation;
+    Vector2 _currentAngles;
+
+    CursorLockMode _previousLockState;
+    bool _wasCursorVisible;
+
+    void Start()
+    { 
+
+    }
+
+    void OnEnable()
+    {
+        _initialOrientation = transform.localRotation;
+
+        _previousLockState = Cursor.lockState;
+        _wasCursorVisible = Cursor.visible;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+        Rigid.MoveRotation(Rigid.rotation * Quaternion.Euler(0, Input.GetAxis("Mouse X") * MouseSensitivity, 0));
+        //Debug.Log(this.playerCamera.transform.rotation.x);
+
+        if (this.playerCamera.transform.rotation.x <= -0.68f)
+        {
+            this.playerCamera.transform.Rotate(-0.67f, 0, 0);
+        }
+        else if (this.playerCamera.transform.rotation.x >= 0.68f)
+        {
+            this.playerCamera.transform.Rotate(0.67f, 0, 0);
+        }
+        else
+        {
+            this.playerCamera.transform.Rotate((Input.GetAxis("Mouse Y") * MouseSensitivity) * -1, 0, 0);
+        }
+
+        Rigid.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * MoveSpeed) + (transform.right * Input.GetAxis("Horizontal") * MoveSpeed));
+
+        //if (Input.GetKeyDown("space"))
+        //{
+        //    Rigid.AddForce(transform.up * JumpForce);
+        //}
+    }
+
+    void OnDisable()
+    {
+
+        Cursor.visible = _wasCursorVisible;
+        Cursor.lockState = _previousLockState;
+        transform.localRotation = _initialOrientation;
+    }
+
+
+
+}
+
